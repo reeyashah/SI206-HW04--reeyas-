6afcd1999fec9ea8c39e6f44e4f092afde1bbc40
Reeya Shah <reeyashah@0587343634.wireless.umich.net>
2018-01-30 15:06:16 -0500

    response

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,6 @@
 def user_question:
     response = input("What is your question?")
+    while response != "quit":
+        if response[-1] != '?':
+            print("I'm sorry, I can only answer questions")
+        response = input("What is your question?")
